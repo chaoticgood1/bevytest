c229a3a1b090404af0672f9d8a4e843d8a35b615
nickan <colete_nico@yahoo.com>
2020-11-19 00:00:13 +0800

    Rendering terrain without shader intervention

--- src/bevy_test.rs
+++ src/bevy_test.rs
@@ -51,355 +51,92 @@ struct MyMaterial {
 const VERTEX_SHADER: &str = r#"
 #version 450
 
+layout(location = 0) in vec3 Vertex_Position;
+layout(location = 1) in vec3 Vertex_Normal;
+layout(location = 2) in vec2 Vertex_Uv;
+
+layout(location = 0) out vec3 v_Position;
+layout(location = 1) out vec3 v_Normal;
+layout(location = 2) out vec2 v_Uv;
+
 layout(set = 0, binding = 0) uniform Camera {
-  mat4 ViewProj;
+    mat4 ViewProj;
 };
 
-// Copied from amethyst_rendy
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec4 material_weights;
-layout(location = 2) in vec3 normal;
-layout(location = 3) in mat4 model; // instance rate
-layout(location = 7) in vec4 tint; // instance rate
-
-layout(location = 0) out vec3 out_position;
-layout(location = 1) out vec3 out_normal;
-layout(location = 2) out vec4 out_color;
-layout(location = 3) out vec4 out_material_weights;
+layout(set = 2, binding = 0) uniform Transform {
+    mat4 Model;
+};
 
 void main() {
-    vec4 vertex_position = model * vec4(position, 1.0);
-    out_position = vertex_position.xyz;
-    out_normal = normalize(mat3(model) * normal);
-    // out_color = tint;
-    out_color = vec4(1.0, 1.0, 1.0, 1.0);
-    out_material_weights = vec4(1.0, 1.0, 1.0, 1.0);
-    // out_material_weights = material_weights / dot(material_weights, vec4(1.0));
-    gl_Position = ViewProj * vertex_position;
+    v_Normal = (Model * vec4(Vertex_Normal, 1.0)).xyz;
+    v_Normal = mat3(Model) * Vertex_Normal;
+    v_Position = (Model * vec4(Vertex_Position, 1.0)).xyz;
+    v_Uv = Vertex_Uv;
+    gl_Position = ViewProj * vec4(v_Position, 1.0);
 }
 "#;
 
 const FRAGMENT_SHADER: &str = r#"
 #version 450
 
-// Copied from amethyst_rendy, augmented for triplanar mapping
+const int MAX_LIGHTS = 10;
 
-const float PI = 3.14159265359;
-
-struct UvOffset {
-    vec2 u_offset;
-    vec2 v_offset;
+struct Light {
+    mat4 proj;
+    vec4 pos;
+    vec4 color;
 };
 
-float tex_coord(float coord, vec2 offset) {
-    return offset.x + coord * (offset.y - offset.x);
-}
+layout(location = 0) in vec3 v_Position;
+layout(location = 1) in vec3 v_Normal;
+layout(location = 2) in vec2 v_Uv;
 
-vec2 tex_coords(vec2 coord, UvOffset offset) {
-    return vec2(tex_coord(coord.x, offset.u_offset), tex_coord(coord.y, offset.v_offset));
-}
+layout(location = 0) out vec4 o_Target;
 
-vec3 schlick_fresnel(float HdotV, vec3 fresnel_base) {
-    return fresnel_base + (1.0 - fresnel_base) * pow(1.0 - HdotV, 5.0);
-}
-
-float ggx_normal_distribution(vec3 N, vec3 H, float a) {
-    float a2 = a * a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
-
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-
-    return (a2 + 0.0000001) / denom;
-}
-
-float ggx_geometry(float NdotV, float NdotL, float r2) {
-    float a1 = r2 + 1.0;
-    float k = a1 * a1 / 8.0;
-    float denom = NdotV * (1.0 - k) + k;
-    float ggx1 = NdotV / denom;
-    denom = NdotL * (1.0 - k) + k;
-    float ggx2 = NdotL / denom;
-    return ggx1 * ggx2;
-}
-
-float s_curve (float x) {
-		x = x * 2.0 - 1.0;
-		return -x * abs(x) * 0.5 + x + 0.5;
-}
-
-struct PointLight {
-    vec3 position;
-    vec3 color;
-    float intensity;
+layout(set = 0, binding = 0) uniform Camera {
+    mat4 ViewProj;
 };
 
-struct DirectionalLight {
-    vec3 color;
-    float intensity;
-    vec3 direction;
+layout(set = 1, binding = 0) uniform Lights {
+    uvec4 NumLights;
+    Light SceneLights[MAX_LIGHTS];
 };
 
-struct SpotLight {
-    vec3 position;
-    vec3 color;
-    vec3 direction;
-    float angle;
-    float intensity;
-    float range;
-    float smoothness;
+layout(set = 3, binding = 0) uniform MyMaterial_albedo {
+    vec4 Albedo;
 };
 
-layout(std140, set = 0, binding = 1) uniform Environment {
-    vec3 ambient_color;
-    vec3 camera_position;
-    int point_light_count;
-    int directional_light_count;
-    int spot_light_count;
-};
-
-layout(std140, set = 0, binding = 2) uniform PointLights {
-    PointLight plight[128];
-};
-
-layout(std140, set = 0, binding = 3) uniform DirectionalLights {
-    DirectionalLight dlight[16];
-};
-
-layout(std140, set = 0, binding = 4) uniform SpotLights {
-    SpotLight slight[128];
-};
-
-layout(std140, set = 1, binding = 0) uniform Material {
-    UvOffset uv_offset;
-    float alpha_cutoff;
-};
-
-layout(set = 1, binding = 1) uniform texture2D MyMaterial_albedo_texture;
-layout(set = 1, binding = 2) uniform texture2D emission_samp;
-layout(set = 1, binding = 3) uniform texture2D normal_samp;
-layout(set = 1, binding = 4) uniform texture2D metallic_roughness_samp;
-layout(set = 1, binding = 5) uniform texture2D ambient_occlusion_samp;
-layout(set = 1, binding = 6) uniform texture2D cavity_samp;
-
+# ifdef MYMATERIAL_ALBEDO_TEXTURE
+layout(set = 3, binding = 1) uniform texture2D MyMaterial_albedo_texture;
 layout(set = 3, binding = 2) uniform sampler MyMaterial_albedo_texture_sampler;
-
-layout(location = 0) in vec3 in_position;
-layout(location = 1) in vec3 in_normal;
-layout(location = 2) in vec4 in_color;
-layout(location = 3) in vec4 in_material_weights;
-
-
-layout(location = 0) out vec4 out_color;
-
-vec3 fresnel(float HdotV, vec3 fresnel_base) {
-  return fresnel_base + (1.0 - fresnel_base) * pow(1.0 - HdotV, 5.0);
-}
-
-vec3 compute_light(vec3 attenuation,
-                 vec3 light_color,
-                 vec3 view_direction,
-                 vec3 light_direction,
-                 vec3 albedo,
-                 vec3 normal,
-                 float roughness2,
-                 float metallic,
-                 vec3 fresnel_base) {
-
-  vec3 halfway = normalize(view_direction + light_direction);
-  float normal_distribution = ggx_normal_distribution(normal, halfway, roughness2);
-
-  float NdotV = max(dot(normal, view_direction), 0.0);
-  float NdotL = max(dot(normal, light_direction), 0.0);
-  float HdotV = max(dot(halfway, view_direction), 0.0);
-  float geometry = ggx_geometry(NdotV, NdotL, roughness2);
-
-
-  vec3 fresnel = fresnel(HdotV, fresnel_base);
-  vec3 diffuse = vec3(1.0) - fresnel;
-  diffuse *= 1.0 - metallic;
-
-  vec3 nominator = normal_distribution * geometry * fresnel;
-  float denominator = 4 * NdotV * NdotL + 0.0001;
-  vec3 specular = nominator / denominator;
-
-  vec3 resulting_light = (diffuse * albedo / PI + specular) * light_color * attenuation * NdotL;
-  return resulting_light;
-}
-
-vec4 triplanar_texture(texture2D samp, float layer, vec3 blend, vec2 uv_x, vec2 uv_y, vec2 uv_z) {
-  vec4 x = texture(sampler2D(samp, MyMaterial_albedo_texture_sampler), uv_x);
-  vec4 y = texture(sampler2D(samp, MyMaterial_albedo_texture_sampler), uv_y);
-  vec4 z = texture(sampler2D(samp, MyMaterial_albedo_texture_sampler), uv_z);
-  return blend.x * x + blend.y * y + blend.z * z;
-}
-
-vec3 triplanar_normal_to_world(texture2D samp, float layer, vec3 blend, vec2 uv_x, vec2 uv_y, vec2 uv_z, vec3 surf_normal) {
-  // Important that the texture is loaded as Unorm.
-  vec3 tnormalx = 2.0 * texture(sampler2D(samp, MyMaterial_albedo_texture_sampler), uv_x).rgb - 1.0;
-  vec3 tnormaly = 2.0 * texture(sampler2D(samp, MyMaterial_albedo_texture_sampler), uv_y).rgb - 1.0;
-  vec3 tnormalz = 2.0 * texture(sampler2D(samp, MyMaterial_albedo_texture_sampler), uv_z).rgb - 1.0;
-
-  // Use swizzle method to convert normal into world space.
-  // Get the sign (-1 or 1) of the surface normal
-  vec3 axis_sign = sign(surf_normal);
-  // Flip tangent normal z to account for surface normal facing
-  tnormalx.z *= axis_sign.x;
-  tnormaly.z *= axis_sign.y;
-  tnormalz.z *= axis_sign.z;
-  // Swizzle tangent normals to match world orientation and triblend
-  return normalize(
-      tnormalx.zyx * blend.x +
-      tnormaly.xzy * blend.y +
-      tnormalz.xyz * blend.z
-  );
-}
-
-vec4 triplanar_texture_splatted(texture2D samp, vec4 mtl_weights, vec3 blend, vec2 uv_x, vec2 uv_y, vec2 uv_z) {
-  vec4 v0 = triplanar_texture(samp, 0.0, blend, uv_x, uv_y, uv_z);
-  vec4 v1 = triplanar_texture(samp, 1.0, blend, uv_x, uv_y, uv_z);
-  vec4 v2 = triplanar_texture(samp, 2.0, blend, uv_x, uv_y, uv_z);
-  vec4 v3 = triplanar_texture(samp, 3.0, blend, uv_x, uv_y, uv_z);
-  // TODO: depth maps
-  return mtl_weights.r * v0 +
-         mtl_weights.g * v1 +
-         mtl_weights.b * v2 +
-         mtl_weights.a * v3;
-}
-
-vec3 triplanar_normal_to_world_splatted(texture2D samp, vec4 mtl_weights, vec3 blend, vec2 uv_x, vec2 uv_y, vec2 uv_z, vec3 surf_normal) {
-  vec3 v0 = triplanar_normal_to_world(samp, 0.0, blend, uv_x, uv_y, uv_z, surf_normal);
-  vec3 v1 = triplanar_normal_to_world(samp, 1.0, blend, uv_x, uv_y, uv_z, surf_normal);
-  vec3 v2 = triplanar_normal_to_world(samp, 2.0, blend, uv_x, uv_y, uv_z, surf_normal);
-  vec3 v3 = triplanar_normal_to_world(samp, 3.0, blend, uv_x, uv_y, uv_z, surf_normal);
-  // TODO: depth maps
-  return normalize(
-      mtl_weights.r * v0 +
-      mtl_weights.g * v1 +
-      mtl_weights.b * v2 +
-      mtl_weights.a * v3
-  );
-}
+# endif
 
 void main() {
-  // Do triplanar mapping (world space -> UVs).
-  float texture_scale = 10.0;
-  vec3 blend = pow(abs(in_normal), vec3(3));
-  blend = blend / (blend.x + blend.y + blend.z);
-  vec2 uv_x = tex_coords(in_position.zy / texture_scale, uv_offset);
-  vec2 uv_y = tex_coords(in_position.xz / texture_scale, uv_offset);
-  vec2 uv_z = tex_coords(in_position.xy / texture_scale, uv_offset);
+    vec4 output_color = Albedo;
+# ifdef MYMATERIAL_ALBEDO_TEXTURE
+    output_color *= texture(
+        sampler2D(MyMaterial_albedo_texture, MyMaterial_albedo_texture_sampler),
+        v_Uv);
+# endif
 
-  vec4 albedo_alpha       = triplanar_texture_splatted(MyMaterial_albedo_texture, in_material_weights, blend, uv_x, uv_y, uv_z);
-  float alpha             = albedo_alpha.a;
-  if(alpha < alpha_cutoff) discard;
+# ifdef MYMATERIAL_SHADED
+    vec3 normal = normalize(v_Normal);
+    vec3 ambient = vec3(0.05, 0.05, 0.05);
+    // accumulate color
+    vec3 color = ambient;
+    for (int i=0; i<int(NumLights.x) && i<MAX_LIGHTS; ++i) {
+        Light light = SceneLights[i];
+        // compute Lambertian diffuse term
+        vec3 light_dir = normalize(light.pos.xyz - v_Position);
+        float diffuse = max(0.0, dot(normal, light_dir));
+        // add light contribution
+        color += diffuse * light.color.xyz;
+    }
+    output_color.xyz *= color;
+# endif
 
-  vec3 albedo             = albedo_alpha.rgb;
-  vec3 emission           = triplanar_texture_splatted(emission_samp, in_material_weights, blend, uv_x, uv_y, uv_z).rgb;
-  vec3 normal             = triplanar_normal_to_world_splatted(normal_samp, in_material_weights, blend, uv_x, uv_y, uv_z, in_normal);
-  vec2 metallic_roughness = triplanar_texture_splatted(metallic_roughness_samp, in_material_weights, blend, uv_x, uv_y, uv_z).bg;
-  float ambient_occlusion = triplanar_texture_splatted(ambient_occlusion_samp, in_material_weights, blend, uv_x, uv_y, uv_z).r;
-  // TODO: Use cavity
-  // float cavity            = texture(cavity, tex_coords(vertex.tex_coord, final_tex_coords).r;
-  float metallic          = metallic_roughness.r;
-  float roughness         = metallic_roughness.g;
-
-  float roughness2 = roughness * roughness;
-  vec3 fresnel_base = mix(vec3(0.04), albedo, metallic);
-
-  vec3 view_direction = normalize(camera_position - in_position);
-  vec3 lighted = vec3(0.0);
-  for (int i = 0; i < point_light_count; i++) {
-      vec3 light_direction = normalize(plight[i].position - in_position);
-      float attenuation = plight[i].intensity / dot(light_direction, light_direction);
-
-      vec3 light = compute_light(vec3(attenuation),
-                                 plight[i].color,
-                                 view_direction,
-                                 light_direction,
-                                 albedo,
-                                 normal,
-                                 roughness2,
-                                 metallic,
-                                 fresnel_base);
-
-      lighted += light;
-  }
-
-  for (int i = 0; i < directional_light_count; i++) {
-      vec3 light_direction = -normalize(dlight[i].direction);
-      float attenuation = dlight[i].intensity;
-
-      vec3 light = compute_light(vec3(attenuation),
-                                 dlight[i].color,
-                                 view_direction,
-                                 light_direction,
-                                 albedo,
-                                 normal,
-                                 roughness2,
-                                 metallic,
-                                 fresnel_base);
-
-      lighted += light;
-  }
-
-  for (int i = 0; i < spot_light_count; i++) {
-      vec3 light_vec = slight[i].position - in_position;
-      vec3 normalized_light_vec = normalize(light_vec);
-
-      // The distance between the current fragment and the "core" of the light
-      float light_length = length(light_vec);
-
-      // The allowed "length", everything after this won't be lit.
-      // Later on we are dividing by this range, so it can't be 0
-      float range = max(slight[i].range, 0.00001);
-
-      // get normalized range, so everything 0..1 could be lit, everything else can't.
-      float normalized_range = light_length / max(0.00001, range);
-
-      // The attenuation for the "range". If we would only consider this, we'd have a
-      // point light instead, so we need to also check for the spot angle and direction.
-      float range_attenuation = max(0.0, 1.0 - normalized_range);
-
-      // this is actually the cosine of the angle, so it can be compared with the
-      // "dotted" frag_angle below a lot cheaper.
-      float spot_angle = max(slight[i].angle, 0.00001);
-      vec3 spot_direction = normalize(slight[i].direction);
-      float smoothness = 1.0 - slight[i].smoothness;
-
-      // Here we check if the current fragment is within the "ring" of the spotlight.
-      float frag_angle = dot(spot_direction, -normalized_light_vec);
-
-      // so that the ring_attenuation won't be > 1
-      frag_angle = max(frag_angle, spot_angle);
-
-      // How much is this outside of the ring? (let's call it "rim")
-      // Also smooth this out.
-      float rim_attenuation = pow(max((1.0 - frag_angle) / (1.0 - spot_angle), 0.00001), smoothness);
-
-      // How much is this inside the "ring"?
-      float ring_attenuation = 1.0 - rim_attenuation;
-
-      // combine the attenuations and intensity
-      float attenuation = range_attenuation * ring_attenuation * slight[i].intensity;
-
-      vec3 light = compute_light(vec3(attenuation),
-                                 slight[i].color,
-                                 view_direction,
-                                 normalize(light_vec),
-                                 albedo,
-                                 normal,
-                                 roughness2,
-                                 metallic,
-                                 fresnel_base);
-      lighted += light;
-  }
-
-  vec3 ambient = ambient_color * albedo * ambient_occlusion;
-  vec3 color = ambient + lighted + emission;
-
-  out_color = vec4(color, alpha) * in_color;
+    // multiply the light by material color
+    o_Target = output_color;
 }
 
 "#;
@@ -609,8 +346,8 @@ fn generate_meshes(
       .add_node_edge("my_material", base::node::MAIN_PASS)
       .unwrap();
 
-  let texture_handle = asset_server.load("albedo.png");
-  // let texture_handle = asset_server.load("elon.png");
+  // let texture_handle = asset_server.load("albedo.png");
+  let texture_handle = asset_server.load("elon.png");
 
   let mutated_chunk_keys = &terrain_resource.mutated_chunk_keys;
   let map = &terrain_resource.map;
@@ -677,14 +414,14 @@ fn generate_meshes(
         // NOTE: in the future you wont need to manually declare dynamic bindings
         PipelineSpecialization {
           dynamic_bindings: vec![
-            DynamicBinding {
-              bind_group: 0,
-              binding: 0,
-            },
-            DynamicBinding {
-              bind_group: 1,
-              binding: 1,
-            },
+            // DynamicBinding {
+            //   bind_group: 0,
+            //   binding: 0,
+            // },
+            // DynamicBinding {
+            //   bind_group: 1,
+            //   binding: 1,
+            // },
             // Transform
             DynamicBinding {
               bind_group: 2,
@@ -706,25 +443,6 @@ fn generate_meshes(
     .with(body)
     .with(collider)
     ;
-
-
-    // Create as a function
-    // spawn_terrain(commands, materials, materials, terrain_resource);
-    // let material = materials.add(
-    //   Color::rgba(0.5, 0.4, 0.3, 1.0).into());
-    // let _entity_ = commands
-    //   .spawn(PbrComponents {
-    //     mesh,
-    //     material: material,
-    //     ..Default::default()
-    //   })
-    //   .with(body)
-    //   .with(collider)
-    //   .with(PickableMesh::default())
-    //   // .with(HighlightablePickMesh::default())
-    //   // .with(SelectablePickMesh::new())
-    //   .current_entity();
-    // terrain_resource.map_chunk_graphics.insert(chunk_key, entity);
   }
 }
 
@@ -741,29 +459,6 @@ fn create_mesh(
   mesh.set_indices(Some(Indices::U32(indices)));
   meshes.add(mesh)
 }
-
-// fn spawn_terrain(
-//   mut commands: Commands,
-//   mut meshes: ResMut<Assets<Mesh>>,
-//   mut materials: ResMut<Assets<StandardMaterial>>,
-
-//   mut terrain_resource: ResMut<TerrainResource>,
-// ) {
-//   let material = materials.add(
-//     Color::rgba(0.5, 0.4, 0.3, 1.0).into());
-//   let _entity_ = commands
-//     .spawn(PbrComponents {
-//       mesh,
-//       material: material,
-//       ..Default::default()
-//     })
-//     .with(body)
-//     .with(collider)
-//     .with(PickableMesh::default())
-//     // .with(HighlightablePickMesh::default())
-//     // .with(SelectablePickMesh::new())
-//     .current_entity();
-// }
 
 fn convert_flatu32_to_points(array: Vec<u32>) -> Vec<Point<u32>> {
   let mut points: Vec<Point<u32>> = Vec::new();
