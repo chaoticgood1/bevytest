e48d689ca661f0467245c5087eab6715afb08aea
nickan <colete_nico@yahoo.com>
2021-02-02 09:33:41 +0800

    Skybox not working on wasm caused by image.rs line 126 or 127

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ fn main() {
     let mut app = App::build();
 
     app
-      // .add_plugin(bevy_webgl2::WebGL2Plugin)
       .add_resource(Msaa { samples: 4 })
       .add_plugins(DefaultPlugins)
       .add_plugin(skybox::CustomPlugin)
+      .add_plugin(bevy_webgl2::WebGL2Plugin)
       .run();
 }
