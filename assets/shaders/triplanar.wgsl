9985dac5bd13dc6f0a7f891623a1f6235b71477c
Nico <colete_nico@yahoo.com>
2023-05-16 09:34:37 +0800

    Save point

--- assets/shaders/triplanar.wgsl
+++ assets/shaders/triplanar.wgsl
@@ -1,5 +1,6 @@
 #import bevy_pbr::mesh_view_bindings
 #import bevy_pbr::mesh_bindings
+#import bevy_pbr::mesh_functions
 
 #import bevy_pbr::pbr_types
 #import bevy_pbr::utils
@@ -10,53 +11,50 @@
 #import bevy_pbr::pbr_functions
 #import bevy_pbr::pbr_ambient
 
+
 @group(1) @binding(0)
-var my_array_texture: texture_2d_array<f32>;
+var albedo: texture_2d_array<f32>;
 @group(1) @binding(1)
-var my_array_texture_sampler: sampler;
+var albedo_sampler: sampler;
+
+
+// struct Vertex {
+//   @location(0) position: vec3<f32>,
+//   @location(1) normal: vec3<f32>,
+// };
+
+// struct VertexOutput {
+//   @builtin(position) clip_position: vec4<f32>,
+//   @location(0) world_position: vec4<f32>,
+//   @location(1) world_normal: vec3<f32>,
+// };
+
+// @vertex
+// fn vertex(vertex: Vertex) -> VertexOutput {
+//   var out: VertexOutput;
+//   out.world_position = mesh_position_local_to_world(mesh.model, vec4<f32>(vertex.position, 1.0));
+//   out.clip_position = mesh_position_local_to_clip(mesh.model, vec4<f32>(vertex.position, 1.0));
+//   out.world_normal = vertex.normal;
+//   return out;
+// }
 
 struct FragmentInput {
-    @builtin(front_facing) is_front: bool,
-    @builtin(position) frag_coord: vec4<f32>,
-    // #import bevy_pbr::mesh_vertex_output
+  // @builtin(position) frag_coord: vec4<f32>,
+  @builtin(front_facing) is_front: bool,
+  @builtin(position) frag_coord: vec4<f32>,
+
+  // @location(0) world_position: vec4<f32>,
+  // @location(1) world_normal: vec3<f32>,
+
+  #import bevy_pbr::mesh_vertex_output
 };
 
 @fragment
-fn fragment(in: FragmentInput) -> @location(0) vec4<f32> {
-//     let layer = i32(in.world_position.x) & 0x3;
-
-//     // Prepare a 'processed' StandardMaterial by sampling all textures to resolve
-//     // the material members
-//     var pbr_input: PbrInput = pbr_input_new();
-
-//     pbr_input.material.base_color = textureSample(my_array_texture, my_array_texture_sampler, in.uv, layer);
-// #ifdef VERTEX_COLORS
-//     pbr_input.material.base_color = pbr_input.material.base_color * in.color;
-// #endif
-
-//     pbr_input.frag_coord = in.frag_coord;
-//     pbr_input.world_position = in.world_position;
-//     pbr_input.world_normal = prepare_world_normal(
-//         in.world_normal,
-//         (pbr_input.material.flags & STANDARD_MATERIAL_FLAGS_DOUBLE_SIDED_BIT) != 0u,
-//         in.is_front,
-//     );
-
-//     pbr_input.is_orthographic = view.projection[3].w == 1.0;
-
-//     pbr_input.N = apply_normal_mapping(
-//         pbr_input.material.flags,
-//         pbr_input.world_normal,
-// #ifdef VERTEX_TANGENTS
-// #ifdef STANDARDMATERIAL_NORMAL_MAP
-//         in.world_tangent,
-// #endif
-// #endif
-//         in.uv,
-//     );
-//     pbr_input.V = calculate_view(in.world_position, pbr_input.is_orthographic);
-
-//     return tone_mapping(pbr(pbr_input));
-
-  return vec4<f32>(0.0, 0.0, 0.0, 1.0);
+fn fragment(input: FragmentInput) -> @location(0) vec4<f32> {
+    var pbr_input: PbrInput = pbr_input_new();
+    return tone_mapping(pbr(pbr_input));
 }
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,122 @@
-use bevy::{
-  asset::LoadState,
-  prelude::*,
-  reflect::TypeUuid,
-  render::render_resource::{AsBindGroup, ShaderRef},
-};
-
+use bevy::{prelude::*, render::{mesh::{MeshVertexAttribute, MeshVertexBufferLayout, Indices}, render_resource::{VertexFormat, AsBindGroup, ShaderRef, SpecializedMeshPipelineError, RenderPipelineDescriptor, PrimitiveTopology}}, reflect::TypeUuid, pbr::{MaterialPipeline, MaterialPipelineKey}, asset::LoadState};
 /// This example illustrates how to create a texture for use with a `texture_2d_array<f32>` shader
 /// uniform variable.
 fn main() {
   App::new()
-      .add_plugins(DefaultPlugins)
-      .add_plugin(MaterialPlugin::<ArrayTextureMaterial>::default())
-      .add_startup_system(setup)
-      .add_system(create_array_texture)
-      .run();
+    .add_plugins(DefaultPlugins)
+    .add_plugin(MaterialPlugin::<CustomMaterial>::default())
+    .add_startup_system(startup)
+    .add_system(init_textures)
+    .run();
 }
 
-#[derive(Resource)]
-struct LoadingTexture {
-  is_loaded: bool,
-  handle: Handle<Image>,
-}
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-  // Start loading the texture.
-  commands.insert_resource(LoadingTexture {
-      is_loaded: false,
-      handle: asset_server.load("textures/array_texture.png"),
+
+fn startup(
+  mut commands: Commands, 
+  asset_server: Res<AssetServer>,
+) {
+  commands.insert_resource(ChunkTexture {
+    is_loaded: false,
+    albedo: asset_server.load("textures/array_texture.png"),
   });
 
   // light
   commands.spawn(PointLightBundle {
-      point_light: PointLight {
-          intensity: 3000.0,
-          ..Default::default()
-      },
-      transform: Transform::from_xyz(-3.0, 2.0, -1.0),
+    point_light: PointLight {
+      intensity: 3000.0,
       ..Default::default()
+    },
+    transform: Transform::from_xyz(-3.0, 2.0, -1.0),
+    ..Default::default()
   });
   commands.spawn(PointLightBundle {
-      point_light: PointLight {
-          intensity: 3000.0,
-          ..Default::default()
-      },
-      transform: Transform::from_xyz(3.0, 2.0, 1.0),
+    point_light: PointLight {
+      intensity: 3000.0,
       ..Default::default()
+    },
+    transform: Transform::from_xyz(3.0, 2.0, 1.0),
+    ..Default::default()
   });
 
   // camera
   commands.spawn(Camera3dBundle {
-      transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::new(1.5, 0.0, 0.0), Vec3::Y),
-      ..Default::default()
+    transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::new(1.5, 0.0, 0.0), Vec3::Y),
+    ..Default::default()
   });
 }
-
-fn create_array_texture(
+  
+fn init_textures(
   mut commands: Commands,
-  asset_server: Res<AssetServer>,
-  mut loading_texture: ResMut<LoadingTexture>,
-  mut images: ResMut<Assets<Image>>,
   mut meshes: ResMut<Assets<Mesh>>,
-  mut materials: ResMut<Assets<ArrayTextureMaterial>>,
+  mut custom_materials: ResMut<Assets<CustomMaterial>>,
+  mut _materials: ResMut<Assets<StandardMaterial>>,
+  asset_server: Res<AssetServer>,
+
+  mut loading_texture: ResMut<ChunkTexture>,
+  mut images: ResMut<Assets<Image>>,
 ) {
   if loading_texture.is_loaded
-      || asset_server.get_load_state(loading_texture.handle.clone()) != LoadState::Loaded
+    || asset_server.get_load_state(loading_texture.albedo.clone()) != LoadState::Loaded
   {
-      return;
+    return;
   }
   loading_texture.is_loaded = true;
-  let image = images.get_mut(&loading_texture.handle).unwrap();
 
-  // Create a new array texture asset from the loaded texture.
   let array_layers = 4;
+  let image = images.get_mut(&loading_texture.albedo).unwrap();
   image.reinterpret_stacked_2d_as_array(array_layers);
 
-  // Spawn some cubes using the array texture
-  let mesh_handle = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
-  let material_handle = materials.add(ArrayTextureMaterial {
-      array_texture: loading_texture.handle.clone(),
+
+  let render_mesh = Mesh::from(shape::Cube { size: 1.0 });
+  let mesh_handle = meshes.add(render_mesh);
+  let material_handle = custom_materials.add(CustomMaterial {
+    albedo: loading_texture.albedo.clone(),
   });
-  for x in -5..=5 {
-      commands.spawn(MaterialMeshBundle {
-          mesh: mesh_handle.clone(),
-          material: material_handle.clone(),
-          transform: Transform::from_xyz(x as f32 + 0.5, 0.0, 0.0),
-          ..Default::default()
-      });
-  }
+
+  commands
+    .spawn(MaterialMeshBundle {
+      mesh: mesh_handle,
+      material: material_handle,
+      transform: Transform::from_xyz(0.0, 0.0, 0.0),
+      ..default()
+    });
+}
+
+#[derive(Resource)]
+struct ChunkTexture {
+  is_loaded: bool,
+  albedo: Handle<Image>,
 }
 
 #[derive(AsBindGroup, Debug, Clone, TypeUuid)]
-#[uuid = "9c5a0ddf-1eaf-41b4-9832-ed736fd26af3"]
-struct ArrayTextureMaterial {
+#[uuid = "5f2e1d29-b8ad-4680-8c96-f8b78a580718"]
+struct CustomMaterial {
   #[texture(0, dimension = "2d_array")]
   #[sampler(1)]
-  array_texture: Handle<Image>,
+  albedo: Handle<Image>,
 }
 
-impl Material for ArrayTextureMaterial {
+impl Material for CustomMaterial {
+  // fn vertex_shader() -> ShaderRef {
+  //   "shaders/triplanar.wgsl".into()
+  // }
   fn fragment_shader() -> ShaderRef {
-      "shaders/triplanar.wgsl".into()
+    "shaders/triplanar.wgsl".into()
   }
+  // fn specialize(
+  //   _pipeline: &MaterialPipeline<Self>,
+  //   descriptor: &mut RenderPipelineDescriptor,
+  //   layout: &MeshVertexBufferLayout,
+  //   _key: MaterialPipelineKey<Self>,
+  // ) -> Result<(), SpecializedMeshPipelineError> {
+  //   // let vertex_layout = layout.get_layout(&[
+  //   //   Mesh::ATTRIBUTE_POSITION.at_shader_location(0),
+  //   //   Mesh::ATTRIBUTE_NORMAL.at_shader_location(1),
+  //   // ])?;
+  //   // descriptor.vertex.buffers = vec![vertex_layout];
+
+  //   Ok(())
+  // }
 }
+  
